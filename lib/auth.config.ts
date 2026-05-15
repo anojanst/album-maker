4dcdee74aac45783ae082e9b494e52ea558cfa60
Anojan ST <ethanojan1@gmail.com>
2026-05-15 23:06:16 +1200

    v2.2.4

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -2,6 +2,7 @@ import type { NextAuthConfig } from 'next-auth'
 
 export const authConfig: NextAuthConfig = {
   secret: process.env.AUTH_SECRET,
+  session: { strategy: 'jwt' },
   pages: {
     signIn: '/sign-in',
   },
