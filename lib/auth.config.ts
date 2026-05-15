8b7444f6d8e2ae4afafa29063a5031989360ac1f
Anojan ST <ethanojan1@gmail.com>
2026-05-15 23:12:19 +1200

    v2.2.5

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -10,6 +10,14 @@ export const authConfig: NextAuthConfig = {
     authorized({ auth }) {
       return !!auth?.user
     },
+    jwt({ token, user }) {
+      if (user?.id) token.id = user.id
+      return token
+    },
+    session({ session, token }) {
+      if (token.id) session.user.id = token.id as string
+      return session
+    },
   },
   providers: [],
 }
