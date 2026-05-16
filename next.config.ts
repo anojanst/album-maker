86989d2302377e7a6eeade3f3f39bb45806787de
Anojan ST <ethanojan1@gmail.com>
2026-05-17 11:37:04 +1200

    v2.3.3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pub-3fd7021ca25b4b06b592f8cf7b1b425e.r2.dev',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
